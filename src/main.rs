c192626b123edb98458516adbe43832fbcce94f6
Tosei <359387050@qq.com>
2023-08-17 15:50:52 +0900

    add rays, a simple camera and background

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+mod ray;
 mod vec;
 
 use std::io::{stderr, Write};
 
-use crate::vec::Color;
+use ray::Ray;
+use vec::Color;
+
+use crate::vec::{Point3, Vec3};
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction().normalize();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+}
 
 fn main() {
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_HEIGHT: u64 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u64;
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    // 焦距
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -16,11 +38,12 @@ fn main() {
         eprint!("\rScanlines remaining: {:3}", IMAGE_HEIGHT - j - 1);
         stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (i as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                0.25,
-            );
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+
+            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let pixel_color = ray_color(&r);
+
             println!("{}", pixel_color.format_color());
         }
     }
--- src/vec.rs
+++ src/vec.rs
@@ -1,3 +1,10 @@
+use core::fmt;
+use std::{
+    fmt::Display,
+    ops::{Add, Div, Index, Mul, Sub},
+};
+
+#[derive(Copy, Clone)]
 pub struct Vec3 {
     e: [f64; 3],
 }
@@ -17,5 +24,104 @@ impl Vec3 {
             (255.999 * self.e[1]) as u64,
             (255.999 * self.e[2]) as u64
         )
+    }
+
+    pub fn x(self) -> f64 {
+        self.e[0]
+    }
+
+    pub fn y(self) -> f64 {
+        self.e[1]
+    }
+
+    pub fn z(self) -> f64 {
+        self.e[2]
+    }
+
+    // 计算向量的点积V1( x1, y1, z1)·V2(x2, y2, z2) = x1*x2 + y1*y2 + z1*z2;
+    pub fn dot(self, other: Vec3) -> f64 {
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2]
+    }
+
+    // 计算向量的长度 |V| = sqrt(V.x^2 + V.y^2 + V.z^2)
+    pub fn length(self) -> f64 {
+        self.dot(self).sqrt()
+    }
+
+    // 计算向量的单位向量
+    pub fn normalize(self) -> Vec3 {
+        self / self.length()
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] + rhs.e[0],
+                self.e[1] + rhs.e[1],
+                self.e[2] + rhs.e[2],
+            ],
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] - rhs.e[0],
+                self.e[1] - rhs.e[1],
+                self.e[2] - rhs.e[2],
+            ],
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs],
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs],
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self * rhs.e[0], self * rhs.e[1], self * rhs.e[2]],
+        }
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &f64 {
+        &self.e[index]
+    }
+}
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {})", self[0], self[1], self[2])
     }
 }
